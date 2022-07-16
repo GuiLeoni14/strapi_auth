60a791c4dbea499614c30b016df7c7cfc3ee93f8
GuiLeoni14 <guileone167@gmail.com>
2022-07-16 19:21:56 -0300

    configurando as policies

--- src/api/post/routes/post.js
+++ src/api/post/routes/post.js
@@ -6,4 +6,14 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::post.post');
+module.exports = createCoreRouter('api::post.post', {
+  config: {
+    create: {
+      policies: ["local-policy", "global::is-logged-in"]
+    },
+    find: {
+      policies: ["local-policy", "global::is-logged-in"]
+    },
+  },
+});
+
